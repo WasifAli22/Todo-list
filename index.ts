4c48686ed3955ad84700b3a9f24ad5f45f579eec
WasifAli22 <Wasifaliwasif22@gmail.com>
2023-01-28 15:35:17 +0500

    It's an ATM Project

---
+++ index.ts
@@ -0,0 +1,70 @@
+import inquirer from "inquirer";
+
+interface ansType {
+    userpin: number,
+    userid: string,
+    accountType: string,
+    transactionType: string,
+    amount: number
+}
+
+const answers: ansType = await inquirer.prompt([
+    {
+        name: "userid",
+        type: "input",
+        message: "Enter Your ID"
+    },
+    {
+        name: "userpin",
+        type: "number",
+        message: "Enter Your PIN"
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["current", "saving"],
+        message: "Seclect your account type: "
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["fastcash", "Withdrawl"],
+        message: "Seclect your transaction",
+        when(answers) {
+            return answers.accountType
+        }
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: [1000, 2000, 10000, 20000],
+        message: "Seclect your amount",
+        when(answers) {
+            return answers.transactionType == "fastcash"
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount",
+        when(answers) {
+            return answers.transactionType == "Withdrawl"
+        }
+    }
+])
+
+if (answers.userid && answers.userpin) {
+    const balance = Math.floor(Math.random() * 100000000)
+    // console.log(balance);
+    const EnteredAmount = answers.amount;
+    if (balance >= EnteredAmount) {
+        const remaining = balance - EnteredAmount;
+        console.log(`Your remaining balance is  ${remaining}`);
+
+    }
+    else {
+        console.log("Insufficent balance");
+
+    }
+
+}
